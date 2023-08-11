d8c78240c5ce539f4fc9e6293bb088580e581a8a
hnguyen-desktop <hnguyen@nuro.ai>
2023-08-11 00:22:12 -0700

    partial function

---
+++ HackerRank/Easy/partial_function.py
@@ -0,0 +1,11 @@
+#Implement a function called partial that takes a function as its first argument, func, followed by an arbitrary number of positional and keyword arguments. The function should return a new function representing the partial application of the provided arguments to the function func. More formally, the function returned should take the remaining arguments if any, and return the value the function func would return if provided with the previous and current arguments.
+
+#!/bin/python3
+
+def partial(func, *args, **kwargs):
+    def inner(*extra_args, **extra_kwargs):
+        all_args = args + extra_args
+        all_kwargs = kwargs.copy()
+        all_kwargs.update(extra_kwargs)
+        return func(*all_args, **all_kwargs)
+    return inner()
